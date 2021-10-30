1dcebd5393e929b2c0eb7e8c34d492fe48bc7995
Roman Scherbakov <romanscherbakov.i@gmail.com>
2021-10-30 19:33:41 +0300

    fixed newton method
    applying newton method to diverging function no longer stalls the computation

--- lab1.cpp
+++ lab1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 const double a = 0, b = 1, eps = 0.1;
+const double PHI = 1.618034;
 
 inline double func(double x) {
     return sqrt(1 + x * x) + exp(-2*x);
@@ -20,6 +21,12 @@ inline double second_d(double x) {
     return -x * x / pow(1 + x * x, 1.5) + 1 / sqrt(1 + x * x) + 4*exp(-2*x);
 }
 
+void cout_min(double a, double b) {
+    cout << "Min is " << min(a, b) << ", "
+         << "between " << a << " @" << a
+         << " and " << b << " @" << b << endl;
+}
+
 void bisection(double a, double b, double eps) {
     double c1, c2, y1, y2;
     if (b - a > 2*eps) {
@@ -29,15 +36,10 @@ void bisection(double a, double b, double eps) {
              << "F(X1) = " << y1 << ", " << "F(X2) = " << y2 << endl;
         (y1 > y2) ? bisection(c1, b, eps) : bisection(a, c2, eps);
     }
-    else {
-        cout << "Min is " << min(func(a), func(b)) << ", "
-             << "between " << func(a) << " @" << a
-             << " and " << func(b) << " @" << b << endl;
-    }
+    else cout_min(func(a), func(b));
 }
 
 void golden_ratio(double a, double b, double eps) {
-    const double PHI = 1.618034;
     double x1, x2, y1, y2;
     if (b - a > eps) {
         x1 = b - (b - a) / PHI; x2 = a + (b - a) / PHI;
@@ -49,34 +51,34 @@ void golden_ratio(double a, double b, double eps) {
              << "F(X2) = " << y2 << endl;
         golden_ratio(a, b, eps);
     }
-    else {
-        cout << "Min is " << min(func(a), func(b)) << ", "
-             << "between " << func(a) << " @" << a
-             << " and " << func(b) << " @" << b << endl;
-    }
+    else cout_min(func(a), func(b));
 }
 
 void secant(double a, double b, double eps) {
-    if (first_d(a) * first_d(b) < 0) {
-        double x = a - first_d(a) / (first_d(a) - first_d(b)) * (a - b);
-        if (abs(first_d(x)) > eps) {
-            cout << "X~ = " << x << " , " << "f'(x~) = " << first_d(x) << endl;
-            (first_d(x) > 0) ? secant(a, x, eps) : secant(x, b, eps);
+    double a_ = first_d(a), b_ = first_d(b);
+    if (a_ * b_ < 0) {
+        double x = a - a_ / (a_ - b_) * (a - b), x_ = first_d(x);
+        if (abs(x_) > eps) {
+            cout << "X~ = " << x << " , " << "f'(x~) = " << x_ << endl;
+            (x_ > 0) ? secant(a, x, eps) : secant(x, b, eps);
         }
         else cout << "Min is " << func(x) << endl;
     }
     else cout << "Function is not suitable for that method" << endl;
 }
 
-void newton(double _, double x0, double eps) {
-    double x = x0 - first_d(x0) / second_d(x0);
-    if (abs(first_d(x)) > eps) {
-        cout << "X = " << x
-             << ", " << "f'(x) = " << first_d(x)
-             << ", " << "f''(x) = " << second_d(x) << endl;
-        newton(_, x, eps);
+void newton(double d, double x0, double eps) {
+    if (d < 40) {
+        double x = x0 - first_d(x0) / second_d(x0);
+        if (abs(first_d(x)) > eps) {
+            cout << "X = " << x
+                 << ", " << "f'(x) = " << first_d(x)
+                 << ", " << "f''(x) = " << second_d(x) << endl;
+            newton(d++, x, eps);
+        }
+        else cout << "Min is " << func(x) << endl;
     }
-    else cout << "Min is " << func(x) << endl;
+    else cout << "Recursion depth exceeded; function does not converge";
 }
 
 void execute(function<void (double, double, double)> f, string name) {
